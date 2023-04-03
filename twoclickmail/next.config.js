a6361dd1e5bbd69870dfab5eb73dcdfcc3b90800
alpaylan <alpkeles99@gmail.com>
2023-04-03 00:04:16 -0400

    fix public url

--- twoclickmail/next.config.js
+++ twoclickmail/next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    PUBLIC_URL: "http://twoclickmail.com",
+    PUBLIC_URL: "http://2clickmail.com",
     SERVER_URL: "https://twoclickmail.fly.dev",
     DEV_PUBLIC_URL: "https://localhost:3000",
     DEV_SERVER_URL: "http://localhost:5000",
