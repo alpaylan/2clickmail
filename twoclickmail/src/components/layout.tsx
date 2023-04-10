decb9127e5ec721dbc5c95b7ea32a4e6ea495aee
alpaylan <alpkeles99@gmail.com>
2023-04-10 17:12:35 -0400

    add page title

--- twoclickmail/src/components/layout.tsx
+++ twoclickmail/src/components/layout.tsx
@@ -2,17 +2,22 @@
 
 import React, { ReactNode } from 'react';
 import Navbar from '@/components/navbar';
+import Head from 'next/head';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  return (
+  return (<>
+    <Head>
+       <title>Two Click Mail</title> 
+    </Head>
     <div>
       <Navbar />
       <main>{children}</main>
     </div>
+    </>
   );
 };
 
